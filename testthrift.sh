f53240130fa19f377b92742c69aca25ba4bf3661
Yuchen Dai <silentdai@gmail.com>
2020-05-14 05:46:15 +0000

    test for h2 thrift
    run
    1. start thrift server and 2 envoy
    sh tstart_client.sh
    sh start_server.sh
    2. run test case
    sh testthrift.sh
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

---
+++ testthrift.sh
@@ -0,0 +1,1 @@
+python3 thrift/driver/client.py  -a 127.0.0.1:9996 -p binary -t framed --headers hname=hvalue add 1 2
--- thrift/driver/testthrift.sh
+++
@@ -1,1 +0,0 @@
-python3 client.py  -a 127.0.0.1:9996 -p binary -t framed --headers hname=hvalue add 1 2
